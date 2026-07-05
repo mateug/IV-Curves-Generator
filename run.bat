ec24c767e2cd2c1b57ceddd2d7c4b1d96ca20732
Mateu Gorrís <mateugorris@gmail.com>
2026-07-05 19:17:37 +0200

    Auto venv installer with a requirements.txt

--- run.bat
+++ run.bat
@@ -1,6 +1,13 @@
 @echo off
 cd /d "%~dp0"
-if exist "venv\Scripts\activate.bat" (
+if not exist "venv\Scripts\activate.bat" (
+    echo Creando entorno virtual por primera vez...
+    python -m venv venv
+    call venv\Scripts\activate.bat
+    echo Instalando dependencias...
+    pip install -r requirements.txt
+) else (
     call venv\Scripts\activate.bat
 )
+
 python main.py
